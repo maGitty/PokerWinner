2175f13771eadddb2e7aceac38438b291e376656
Marcel Herm <mahik18@gmx.de>
2022-09-16 15:22:20 +0200

    Complete example input Hand in application

--- src/main/java/pokerwinner/codefortynine/App.java
+++ src/main/java/pokerwinner/codefortynine/App.java
@@ -14,12 +14,16 @@ public class App
         List<Card> deck1 = List.of(
                 new Card(CardValue.FIVE, CardSuit.C),
                 new Card(CardValue.A, CardSuit.D),
-                new Card(CardValue.SIX, CardSuit.C)
+                new Card(CardValue.SEVEN, CardSuit.C),
+                new Card(CardValue.SEVEN, CardSuit.H),
+                new Card(CardValue.SEVEN, CardSuit.D)
         );
         List<Card> deck2 = List.of(
                 new Card(CardValue.J, CardSuit.C),
-                new Card(CardValue.TWO, CardSuit.H),
-                new Card(CardValue.J, CardSuit.S)
+                new Card(CardValue.J, CardSuit.H),
+                new Card(CardValue.J, CardSuit.S),
+                new Card(CardValue.A, CardSuit.H),
+                new Card(CardValue.A, CardSuit.C)
         );
         Hand hand1 = new Hand(deck1);
         Hand hand2 = new Hand(deck2);
